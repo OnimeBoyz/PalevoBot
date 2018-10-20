ec1ae4ebf67c1a36d4c97704568f5ffe80110d31
Ruslan Humeniuk <rus.gumeniuk@gmail.com>
2018-10-20 15:49:31 +0300

    changed ssl and time

--- src/main/java/edu/palevobot/config/DaoConstants.java
+++ src/main/java/edu/palevobot/config/DaoConstants.java
@@ -3,6 +3,6 @@ package edu.palevobot.config;
 public class DaoConstants {
     public static final String USERNAME = "root";
     public static final String PASSWORD = "root";
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/palevobotdb";
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/palevobotdb?serverTimezone=Europe/Kiev&useSSL=false";
     public static final String NOSQL_MONGO_URL = "mongodb://localhost:27017";
 }

f6c58bd3c8248b6a5d910d947603541cc58bdfe0
Ruslan Humeniuk <rus.gumeniuk@gmail.com>
2018-10-20 14:00:42 +0300

    Refactored argument in getDao(). DaoType instead of String

--- src/main/java/edu/palevobot/dao/DaoFactory.java
+++ src/main/java/edu/palevobot/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package edu.palevobot.dao;
 
+import edu.palevobot.dao.user.DaoType;
 import edu.palevobot.entities.Base;
 
 import java.sql.SQLException;
 
 public abstract class DaoFactory<T extends Base> {
-    public abstract Dao<T> getDao(String type) throws SQLException;
+    public abstract Dao<T> getDao(DaoType type) throws SQLException;
 }

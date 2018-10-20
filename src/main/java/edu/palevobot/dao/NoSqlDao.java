064f723b08c15c261d4b158e921b75608e31396e
Ruslan Humeniuk <rus.gumeniuk@gmail.com>
2018-10-20 14:41:33 +0300

    Refactored ctor

--- src/main/java/edu/palevobot/dao/NoSqlDao.java
+++ src/main/java/edu/palevobot/dao/NoSqlDao.java
@@ -2,19 +2,33 @@ package edu.palevobot.dao;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import edu.palevobot.config.DaoConstants;
 import edu.palevobot.entities.Base;
 import com.mongodb.*;
+import edu.palevobot.entities.Palevo;
+import edu.palevobot.entities.User;
+import edu.palevobot.entities.UserPalevo;
+import org.bson.Document;
+
+import java.util.ArrayList;
+
 public abstract class NoSqlDao<T extends Base> extends Dao<T>{
+
     protected MongoClient mongoClient;
     protected MongoDatabase mongoDatabase;
+
     public NoSqlDao() {
         mongoClient = MongoClients.create(DaoConstants.NOSQL_MONGO_URL);
+        if(mongoClient != null){
+            mongoDatabase = mongoClient.getDatabase("palevobotdb");
+        }
     }
 
-    public void closeMongoClient() {
-        if(mongoClient != null) {
+    @Override
+    public void closeConnection(){
+                if(mongoClient != null) {
             mongoClient.close();
         }
     }

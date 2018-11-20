e0d3e16b06536ae3466a69613cadb29a4741dc9e
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-20 22:27:16 +0200

    sr

--- src/test/java/edu/palevobot/entities/UserTest.java
+++ src/test/java/edu/palevobot/entities/UserTest.java
@@ -1,8 +1,9 @@
 package edu.palevobot.entities;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 class UserTest {
 

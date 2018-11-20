eb80b35bcf96775c5370de21724b5b0fc74b375c
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-20 19:23:11 +0200

    Added method to start Spring

--- src/main/java/edu/palevobot/Main.java
+++ src/main/java/edu/palevobot/Main.java
@@ -1,14 +1,22 @@
 package edu.palevobot;
 
 import edu.palevobot.config.BotConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import static edu.palevobot.config.BotConfig.BOT_USERNAME;
 
+@SpringBootApplication
+@EnableJpaAuditing
 public class Main {
     public static void main(String[] args) {
+      }
+
+    private void startBot(){
         try {
             ApiContextInitializer.init();
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
@@ -21,5 +29,9 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println("PalevoBot successfully started!");
+
+    }
+    private void startApi(String[] args){
+        SpringApplication.run(Main.class, args);
     }
 }

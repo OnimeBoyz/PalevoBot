6bb5eb4278fa139c8d33f921cd34a281d2d0f04e
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-20 18:04:57 +0200

    Added empty ctor

--- src/main/java/edu/palevobot/entities/Base.java
+++ src/main/java/edu/palevobot/entities/Base.java
@@ -5,13 +5,17 @@ import java.sql.Date;
 public class Base {
     private int id;
     private Date dateOfCreation;
+    
     public Base(int id, Date dateOfCreation) {
         this.id = id;
         this.dateOfCreation = dateOfCreation;
     }
-
     public Base(int id){
+        this();
         this.id = id;
+    }
+    public Base(){
+        this.dateOfCreation = new Date(System.currentTimeMillis());
     }
 
     public int getId() {
